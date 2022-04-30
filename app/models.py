749134dde68954aa196c11a1dc7392197d0d36a4
EmmaK <emmaculatewkamau@gmail.com>
2022-04-30 06:34:00 +0300

    Create tests and define class objects

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 '''
 Define and initialize classes for our objects
 '''
+
+class Source:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+    pass
+
+class Article:
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content) -> None:
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+        pass
---
+++ tests/test_sources.py
@@ -0,0 +1,9 @@
+import unittest
+from app.models import Source
+
+class SourcesTest(unittest.TestCase):
+    def setUp(self) -> None:
+        new_source = Source("1234","The New York Times")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
